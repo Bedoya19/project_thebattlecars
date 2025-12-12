196bb5969787efd4e1e8970b4cb815292c76d9fe
Bedoya19 <108559038+Bedoya19@users.noreply.github.com>
2025-12-11 20:55:52 -0500

    Comenzado desarrollo de la pantalla de informacion de la carta seleccionada

--- src/script.js
+++ src/script.js
@@ -7,8 +7,8 @@ const card = {
     name: "Dodge Dart GT",
     description: "El carro mas basico, todo el mundo lo tiene",
     health: 10,
-    attBuff: 0,
     capacity: 4,
+    attBuff: 0,
     nitro: [3, 3],
     "descripcion larga": "El Dodge Dart GT es la carta de carro mas basica y comun de todas. Que sea basica y comun no significa que sea una mala carta, ya que es bastante balanceada, estable en lo que se espera, y combina con muchas estrategias"
 }
