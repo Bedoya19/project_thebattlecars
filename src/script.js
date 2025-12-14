5abd8093490ae89eed98e27d138e19d573a155ed
Bedoya19 <108559038+Bedoya19@users.noreply.github.com>
2025-12-14 13:18:21 -0500

    Comienzo con lo bueno, el desarrollo de la interactividad del juego

--- src/script.js
+++ src/script.js
@@ -1,14 +1,5 @@
-const types = ["car", "weapon", "material", "power"]
+// THE BATTLE CARS
+// Proyecto de pratica de programacion en la web
+// Hecho por Bedoya1905
+// Alpha v0.0.01
 
-const card = {
-    id: 1,
-    category: 1,
-    type: types[0],
-    name: "Dodge Dart GT",
-    description: "El carro mas basico, todo el mundo lo tiene",
-    health: 10,
-    capacity: 4,
-    attBuff: 0,
-    nitro: [3, 3],
-    "descripcion larga": "El Dodge Dart GT es la carta de carro mas basica y comun de todas. Que sea basica y comun no significa que sea una mala carta, ya que es bastante balanceada, estable en lo que se espera, y combina con muchas estrategias"
-}
