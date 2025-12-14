9ef7929bd983b9b6f16bfa629d405c60ab7285ef
Bedoya19 <108559038+Bedoya19@users.noreply.github.com>
2025-12-14 13:28:52 -0500

    Creado enums de las categorias y tipos de carros

--- src/script.js
+++ src/script.js
@@ -3,3 +3,19 @@
 // Hecho por Bedoya1905
 // Alpha v0.0.01
 
+// Enum de categoria
+const Category = Object.freeze({
+    CATEGORY1: Symbol("category 1"),
+    CATEGORY2: Symbol("category 2"),
+    CATEGORY3: Symbol("category 3"),
+    CATEGORY4: Symbol("category 4"),
+    CATEGORY5: Symbol("category 5"),
+    CATEGORY6: Symbol("category 6")
+});
+// Enum de tipos de cartas
+const TypeCard = Object.freeze({
+    CAR: Symbol("car"),
+    WEAPON: Symbol("weapon"),
+    MATERIAL: Symbol("material"),
+    POWER: Symbol("power")
+});
