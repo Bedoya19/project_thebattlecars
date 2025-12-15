3361c7204a16a55f6933571dd3ad6a0df7ab52b3
Bedoya19 <108559038+Bedoya19@users.noreply.github.com>
2025-12-14 20:34:22 -0500

    Agregado clase de WeaponCard

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,7 +21,7 @@ const TypeCard = Object.freeze({
 });
 
 
-// Objeto de guia temporal
+// Objetos de guia temporal
 const card = {
     category: 1,
     //type: types[0],
@@ -31,7 +31,7 @@ const card = {
     capacity: 4,
     attBuff: 0,
     nitro: [3, 3],
-    "descripcion larga": "El Dodge Dart GT es la carta de carro mas basica y comun de todas. Que sea basica y comun no significa que sea una mala carta, ya que es bastante balanceada, estable en lo que se espera, y combina con muchas estrategias"
+    longDescription: "El Dodge Dart GT es la carta de carro mas basica y comun de todas. Que sea basica y comun no significa que sea una mala carta, ya que es bastante balanceada, estable en lo que se espera, y combina con muchas estrategias."
 }
 const card1 = {
     category: 1,
@@ -39,8 +39,19 @@ const card1 = {
     description: "¿Un lanzacohetes de carto? ¿Y eso como se supone que debe funcionar?",
     attacksLv1: [1, 2, 3, 4, 5, 6],
     attacksLv2: [2, 3, 4, 5, 6, 7],
-    attacksLv3: [3, 4, 5, 6, 7, 9]
-
+    attacksLv3: [3, 4, 5, 6, 7, 8],
+    energy: 4,
+    // Cambiar esto despues.
+    materials: [["cardboard", 4], ["rubberband", 2], ["gunpowder", 1]],
+    longDescription: "Por mas que parezca una mala idea, este lanzacohetes de carton es una de las armas mas basicas, pero la mismo tiempo mas robustas de la categoria. Tiene una progresion estable en su ataque, sus mejoras son creibles y lineales, y no es muy caro de mejorar."
+}
+const card2 = {
+    category: 1,
+    type: "material",
+    name: "Carton",
+    materialInQuestion: "cardboard",
+    quantity: 1,
+    description: "Este simple pedazo de carton te puede ayudar a mejorar todas las armas de la categoria 1 a un nivel mayor, mejorando sus habilidades y rellenando su poder en el proceso." 
 }
 
 // Validar categorias y tipos correctos para evitar problemas a futuro
@@ -56,22 +67,35 @@ class ValidateEnums {
 class CarCard {
     static #nextId = 1;
 
-    constructor(category, type, name, description, health, capacity, attBuff, nitro, largeDescription, imagen) {
+    constructor(category, name, description, health, capacity, attBuff, nitro, longDescription, imagen) {
         this.id = `car-${CarCard.#nextId++}`;
         this.category = ValidateEnums(category, Category, "Categoria invalida");
-        this.type = ValidateEnums(type, TypeCard, "Tipo de carta invalida");
+        this.type = TypeCard.CAR
         this.name = name;
         this.description = description;
         this.health = health;
         this.capacity = capacity;
         this.attBuff = attBuff;
         this.nitro = nitro;
-        this.largeDescription = largeDescription;
+        this.longDescription = longDescription;
         this.imagen = imagen;
     }
+
 }
 
 class WeaponCard {
     static #nextId = 1;
 
+    constructor(category, name, description, attacks, energy, materials, longDescription) {
+        this.id = `weapon-${WeaponCard.#nextId++}`;
+        this.category = ValidateEnums(category, Category, "Categoria invalidad");
+        this.type = TypeCard.WEAPON;
+        this.name = name;
+        this.description = description;
+        // Toca revisar que la informacion este correcta
+        this.attacks = attacks;
+        this.energy = energy;
+        this.materials = materials;
+        this.longDescription = longDescription;
+    }
 }
