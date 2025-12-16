cb41c6385153e84ac7d0787e52d4e792102433e4
Bedoya19 <108559038+Bedoya19@users.noreply.github.com>
2025-12-16 13:34:01 -0500

    sera que convierto todos estos datos a JSON para hacer mas facil el mismo proyecto en Java y otros?

--- src/scripts/models/cards/MaterialCard.js
+++ src/scripts/models/cards/MaterialCard.js
@@ -2,13 +2,14 @@ import { Category } from "../constants/enums.js";
 import { TypeCard } from "../constants/enums.js";
 import { ValidateEnums } from "../constants/enums.js";
 import { Card } from "../cards/Card.js";
+import { materials } from "../constants/materials.js";
 
 export class MaterialCard extends Card {
     static #nextId = 1;
 
     constructor(category, name, description, longDescription, image, materialInQuestion, quantity) {
         super(category, TypeCard.MATERIAL, name, description, longDescription, image);
-        
+
         this.materialInQuestion = ValidateEnums.isValidEnum(materialInQuestion, MaterialsCat1, "Material de Categoria 1 invalido");
         this.quantity = quantity;
     }
