3401ea91cd2222693f85b82a0b922cf141445d04
Bedoya19 <108559038+Bedoya19@users.noreply.github.com>
2025-12-18 13:12:26 -0500

    eliminado comentario de las funciones que remplazo la clase

--- src/scripts/display/deckDisplay.js
+++ src/scripts/display/deckDisplay.js
@@ -37,33 +37,3 @@ export class DisplayCardsInDeck {
         }
     }
 }
-
-// Este va a ser un display general, mientras se prueban cosas.
-/*
-export const displayCardOnDeck = (divDeck, card) => {
-    let quantityCardsInDeck = document.querySelectorAll(".deck-card").length;
-
-    // Crea un div de la carta
-    const divCardInDeck = document.createElement("div");
-    divCardInDeck.setAttribute("id", `deck-card-${quantityCardsInDeck}`);
-    divCardInDeck.setAttribute("class", "deck-card")
-    divCardInDeck.insertAdjacentHTML("beforeend", 
-        `<img src="${card.image}" alt="card-${quantityCardsInDeck}">`
-    )
-    divCardInDeck.addEventListener("click", () => {clickOnDeckCard(card)});
-
-    // La agrega al mazo.
-    divDeck.appendChild(divCardInDeck);
-}
-
-// Mostrar todas las cartas de un array al div del deck.
-export const showDeckOfCards = (divDeck, cards) => {
-    if (cards.length !== 0) {
-        for (const card of cards) {
-            displayCardOnDeck(divDeck, card);
-        }
-    } else {
-        // Esto tendra que avisarle al usuario en pantalla, pero eso vendra despues
-        console.log("No existen cartas en este mazo!");
-    }
-}*/
