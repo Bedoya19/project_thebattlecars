aa2369bc0bd912e9628955f25134620827d44e5f
Bedoya19 <108559038+Bedoya19@users.noreply.github.com>
2025-12-19 14:09:36 -0500

    agregado funcion de mostrar la informacion de una carta de arma + muchas funciones para soportar eso

--- src/scripts/display/displayCardInformation.js
+++ src/scripts/display/displayCardInformation.js
@@ -7,6 +7,7 @@ export class DisplayCardInformation {
                 DisplayCardInformation.displayCarCardInformationDeck(divElement, card);
                 break;
             case "weapon":
+                DisplayCardInformation.displayWeaponCardInformationDeck(divElement, card);
                 break;
             case "material":
                 break;
@@ -29,9 +30,17 @@ export class DisplayCardInformation {
         `;
     }
 
+    // Funcion de mostrar la desripcion de la carta
+    static descriptionCardInformation(card) {
+        return `
+            <hr id="card-selected-bar">
+            <p id="card-selected-description" class="card-selected-information">"${card.description}"</p>
+        `
+    }
+
     // Mostrar la informacion de una carta de carro del deck
     static displayCarCardInformationDeck(divElement, card) {
-        console.log(card);
+        //console.log(card);
         // Esto es solo para mostrar la informacion de la carta en el deck.
         const carInformationFormat = `
             ${this.mainCardInformation("Jugador 1", card.image, card.name)}
@@ -50,11 +59,81 @@ export class DisplayCardInformation {
         divElement.innerHTML = carInformationFormat;
     }
     
-    // Funcion de mostrar la desripcion de la carta
-    static descriptionCardInformation(card) {
-        return `
-            <hr id="card-selected-bar">
-            <p id="card-selected-description" class="card-selected-information">"${card.description}"</p>
+    // Mostrar la informacion de una carta de weapon del deck
+    static displayWeaponCardInformationDeck(divElement, card) {
+        const weaponInformationFormat = `
+            ${this.mainCardInformation("Jugador 1", card.image, card.name)}
+            <div id="card-selected-general-information">
+                <div id="card-selected-attacks">
+                    <p class="card-selected-information">Ataques:</p>
+                    <div id="card-selected-attacks">
+                        ${WeaponListStatsDisplay.displayWeaponAttacks(card)}
+                    </div>
+                </div>
+                <p id="card-selected-energy" class="card-selected-information">Energia: <span>4</span></p>
+                <div>
+                    <p class="card-selected-information">Materiales para mejorar:</p>
+                    <div id="card-selected-materials">
+                        ${WeaponListStatsDisplay.displayWeaponMaterials(card)}
+                    </div>
+                </div>
+                ${this.descriptionCardInformation(card)}
+            </div>
         `
+        divElement.innerHTML = weaponInformationFormat;
+    }
+}
+
+// Clase de las funciones de mostrar los ataques y los materiales de cada arma respectiva en un formato de lista, ya preparado para adjuntarlo a cualquier HTML
+// Esta separada de DisplayCardInformation ya que no quiero se exportada por ahora al main.js
+class WeaponListStatsDisplay {
+    // Devolver los ataques de una arma en formato HTML
+    // En todo esto se asume que se da una carta de arma, por ahora no veo factible revisar
+    static displayWeaponAttacks(weaponCard) {
+        const attackList = this.createUlElementForWeapon("attacks");
+
+        for (let i = 0; i < weaponCard.attacks.length ; i++) {
+            // Crea una string bonita de cada elemento de attackValues
+            const attackValues = weaponCard.attacks[i].join(", ");
+        
+            // Creacion de cada elemento de la lista
+            const attackListLi = this.createListElementInWeapon("card-selected-attack");
+            attackListLi.innerHTML = `Nivel ${i + 1}: (${attackValues})`;
+            attackList.appendChild(attackListLi);
+        }
+        return attackList.outerHTML;
+    }
+    // Devolver los materiales de un arma en un formato HTML
+    // Una vez mas, se asumo que se da una carta de arma.
+    static displayWeaponMaterials(weaponCard) {
+        const materialList = this.createUlElementForWeapon("materials");
+
+        // Lo mismo que el anterior, creacion de cada elemento de la lista, esta vez con un formato distinto
+        for (const material of weaponCard.materials) {
+            //console.log(material);
+            // Crea cada elemento de la lista
+            const materialListLi = this.createListElementInWeapon("card-selected-material");
+            materialListLi.innerHTML = `${material[0]}: ${material[1]}`;
+            materialList.appendChild(materialListLi);
+        }
+        return materialList.outerHTML;
+    }
+
+    // Las siguientes funciones son mas internas de las anteriores dos para evitar repetir tanto codigo
+    // Un chingo de funciones estoy haciendo...
+    // Crea de forma mas flexible un elemento li para las listas que usa la informacion de 
+    static createListElementInWeapon(customClass) {
+        const liElement = document.createElement("li");
+        liElement.classList.add("card-selected-information");
+        liElement.classList.add("card-selected-list");
+        liElement.classList.add(customClass);
+        return liElement;
+    }
+    // Crea lo basico del elemento <ul> base para la lista
+    static createUlElementForWeapon(useInList) {
+        const ulElement = document.createElement("ul");
+        ulElement.id = `card-selected-${useInList}-list`;
+        ulElement.classList.add("card-selected-lists");
+        return ulElement;
     }
 }
