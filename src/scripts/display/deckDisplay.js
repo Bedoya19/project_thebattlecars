e7c1092497309c7ef1b8d353d159fd85e132d6ea
Bedoya19 <108559038+Bedoya19@users.noreply.github.com>
2025-12-20 22:54:21 -0500

    se logro hacer mas cambios para las casillas de carro

--- src/scripts/display/deckDisplay.js
+++ src/scripts/display/deckDisplay.js
@@ -16,12 +16,17 @@ export class BoardClick {
         try{
             const cardObj = JSON.parse(cardGeneralInformation.dataset.card);
             console.log(cardObj.type);
+
+            const carBoardPlayer = carSquare.classList[2];
+            const carBoardZone = carSquare.classList[3];
+
+            console.log(carBoardPlayer, carBoardZone);
         }
         catch (e) {
             // Si hubo un error, lo mas probable es que fuera que el JSON estuviera undefined, entonces esto se muestra
             console.log("Ninguna carta seleccionada! Mostrar aqui que la casilla es del jugador X de la zona Y");
         }
-        //console.log(carSquare.classList);
+        
     }
 }
 
