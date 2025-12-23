d7fd864aca60a205436d915b9ad58e17b22e7be6
Bedoya19 <108559038+Bedoya19@users.noreply.github.com>
2025-12-23 14:00:34 -0500

    se puso advertencia del uso que se le tiene que dar a standarizedDocCreation para evitar complicarme la vida en un futuro

--- src/scripts/display/standardDoc/standarizedDocCreaction.js
+++ src/scripts/display/standardDoc/standarizedDocCreaction.js
@@ -1,19 +1,11 @@
-
-const element = {
-    "element": "div",
-    "id": "hola",
-    "classes": ["hola-div", "hola-subdiv"],
-    "otherAttributes": {
-        "alt": "prueba de div"
-    },
-    "content": "hola"
-}
-const element1 = {
-    "element": "div",
-    "attribute1": "value1"
-}
-// Esta clase va a estandarizar la creacion de documentos HTML
-// tal vez ahorre algunas lineas de codigo y ayude a solucionar ciertos errores especificos en partes del codigo
+// Esta clase estandariza la creacion de documentos HTML
+/* 
+NO TODOS, en ciertas situaciones es simplemente mas sencillo tanto para mi como para el futuro del mantenimiento 
+de la pagina simplemente usar retornos de HTML que ponerme a reinventar la rueda cada vez que quiera agregar un 
+simple div.
+Yo recomendaria solo usar estos dos metodos si el elemento HTML es lo suficientemente sencillo para hacerlo por 
+este medio, y que alla una ganancia de ser mas entendible y en ocaciones, ahorrar lineas de codigo.
+*/
 export class StandarizedDocCreation {
     // Crear un elemento con id, clases, y atributos opcionales
     static elementCreator(elementObj) {
@@ -34,7 +26,7 @@ export class StandarizedDocCreation {
         return element;
     }
     // Crea un elemento sin id o classes, solo con algunos atributos personalizados
-    // (Tecnicamente si puede crear elementos con ID y classes, pero es mejor el anterior)
+    // (Tecnicamente si puede crear elementos con ID y classes, pero es mejor el anterior para elementos con id Y clases)
     static customElementCreator(elementObj) {
         const element = document.createElement(elementObj["element"]);
         for (const [key, value] of Object.entries(elementObj)) {
