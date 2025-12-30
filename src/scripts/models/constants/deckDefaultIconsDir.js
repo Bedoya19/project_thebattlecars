fad27a454ecbe3ae02a031a1ded8a7463081d84f
Bedoya19 <108559038+Bedoya19@users.noreply.github.com>
2025-12-30 16:38:17 -0500

    llevado la direccion de las imagenes iconos del deck a su propia clase para poder ser usado en otros lugares

---
+++ src/scripts/models/constants/deckDefaultIconsDir.js
@@ -0,0 +1,12 @@
+/* 
+Algo muy parecido a lo que hice con los jugadores, en este script tendra de una manera mas estandarizada
+en donde se va a guardar informacion de la direccion de las imagenes del mazo seleccionado
+*/
+export class deckDefaultIconsDir {
+    static #deckIcons = {
+        "cars": "src/images/default_icons/default_cars.png",
+        "weapons": "src/images/default_icons/default_weapons.png",
+        "materials": "src/images/default_icons/default_material.png"
+        //"powers": "../images/default_icons/default_power.png"
+    }
+}
