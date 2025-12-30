96a9cdbee534510875b6ddaa6418661d1550f8c5
Bedoya19 <108559038+Bedoya19@users.noreply.github.com>
2025-12-29 22:42:22 -0500

    preparado el HTML para el siguiente paso: agregar funcionalidad al div de estadisticas

---
+++ src/scripts/display/gameStatsDisplay.js
@@ -0,0 +1,7 @@
+/*
+En este script se va a manejar todo lo que tiene que ver con mostrar la informacion en el div de las estadisticas
+del juego
+Esto incluye actualizar los datos del juego en general, como los turnos y las rondas. Tambien maneja los valores
+personales del jugador, que es la cantidad de nitro, poder y "carga" (el ataque que va a hacer el poder), y las
+acciones que se hacen en el tablero.
+*/
