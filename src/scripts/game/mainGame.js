ec40cb98513956d5b77b0de569dab5dc05a1d531
Bedoya19 <108559038+Bedoya19@users.noreply.github.com>
2025-12-30 18:36:00 -0500

    agregado mainGame.js, en donde contentra toda la logica del juego en si

---
+++ src/scripts/game/mainGame.js
@@ -0,0 +1,17 @@
+// El script en donde tendra el ciclo del juego principal.
+// Ahora los objetivos son los siguientes:
+// * Que se cambie de un jugador a otro
+// * Que limite la cantidad de acciones con respecto al poder de cada jugador
+// * El ciclo del juego. Es decir, que siga indefinidamente el cambio de turnos
+
+// Como lo ha sido siempre en este proyecto, se va a crear una clase
+// En general ha sido asi en todo el proyecto para facilitar la importacion y exportacion de funciones hacia
+// otros documentos JS.
+export class MainGame {
+    // Cambia de jugadores
+    static changePlayers() {
+        // Cambia el data del deck para que sea del otro jugador
+        const currentPlayer = document.getElementById("deck").dataset.player;
+        console.log(currentPlayer);
+    }
+}
