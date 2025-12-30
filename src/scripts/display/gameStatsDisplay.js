da54d27293742ad26604755fdfb18d2061cdd7b2
Bedoya19 <108559038+Bedoya19@users.noreply.github.com>
2025-12-30 11:10:15 -0500

    creado un metodo generico para que este mas conssico la creacion de una nota del turno

--- src/scripts/display/gameStatsDisplay.js
+++ src/scripts/display/gameStatsDisplay.js
@@ -12,19 +12,22 @@ export class GameStatsDisplay {
         document.getElementById("turn-notes-content").innerHTML = "";
     }
 
+    // Metodo generico para agregar una anotacion en las notas del turno
+    static addTurnNote(message) {
+        const turnContent = document.getElementById("turn-notets-content");
+        const text = this.createStatsText();
+        text.innerText = message;
+        turnContent.appendChild(text);
+    }
+
     // Agrega en las notas del turno la colocacion de una carta de carro
     static carOnBoardTurnNotes(player, zone, carName) {
-        const turnContent = document.getElementById("turn-notes-content");
-        const text = this.createStatsText();
-        text.innerText = `${player} coloco "${carName}" en la zona ${zone}`;
-        turnContent.appendChild(text);
+        this.addTurnNote(`${player} coloco "${carName}" en la zona ${zone}`);
     }
 
     // Agrega en las notas del turno la colocaccion de una carta de arma
     static weaponOnBoardTurnNotes(player, carName, weaponName) {
-        const turnContent = document.getElementById("turn-notes-content");
-        const text = this.createStatsText();
-        text.innerText = `${player} coloco "${weaponName}" en el carro ${carName}`;
+        this.addTurnNote(`${player} coloco "${weaponName}" en el carro ${carName}`);
     }
 
     // Crear un elemento P. Esto es para evitar escribir la clase del elemento P cada rato
