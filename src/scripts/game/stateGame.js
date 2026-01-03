1d7b8d6a5b668aba0a397e1d66361f28ce4961b1
Bedoya19 <108559038+Bedoya19@users.noreply.github.com>
2026-01-03 16:21:24 -0500

    creado nueva clase, StateGame, que maneja todo lo respecto a la informacion del juego

--- src/scripts/game/stateGame.js
+++ src/scripts/game/stateGame.js
@@ -4,3 +4,26 @@
     La informacion de aqui es usada por clases como mainGame.js para administrar las rondas usnado la informacion respectiva.
 */
 
+// Clase que maneja la informacion del juego
+export class StateGame {
+    // - Variables del estado del juego -
+    // Carros que siguen teniendo cada jugador
+    static #carsPlayer1 = 5;
+    static #carsPlayer2 = 5;
+    // Rondas y turnos actuales en el juego
+    static #round = 1;
+    static #turn = 0;
+
+    static getCarsPlayers1() {
+        return this.#carsPlayer1;
+    }
+    static getCarsPlayers2() {
+        return this.#carsPlayer2;
+    }
+    static getRound() {
+        return this.#round;
+    }
+    static getTurn() {
+        return this.#turn;
+    }
+}
