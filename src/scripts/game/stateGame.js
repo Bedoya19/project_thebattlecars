4453828ce972dd16263bd9a67303a21fea47b3f1
Bedoya19 <108559038+Bedoya19@users.noreply.github.com>
2026-01-03 13:41:01 -0500

    creado nuevo documento stateGame.js que maneja la informacion del juego

---
+++ src/scripts/game/stateGame.js
@@ -0,0 +1,6 @@
+/*
+    Toda la informacion actual del juego. Principalmente, la informacion de los jugadores (cantidad de carros faltantes para cada jugador), y 
+    el turno y ronda respectiva.
+    La informacion de aqui es usada por clases como mainGame.js para administrar las rondas usnado la informacion respectiva.
+*/
+
