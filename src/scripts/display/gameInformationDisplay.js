70baceb4fc98f7acfab90baff504c65b6b93df06
Bedoya19 <108559038+Bedoya19@users.noreply.github.com>
2026-01-03 20:40:16 -0500

    creado los respectivos metodos individuales de actualizar los datos de la informacion general del juego

--- src/scripts/display/gameInformationDisplay.js
+++ src/scripts/display/gameInformationDisplay.js
@@ -7,6 +7,7 @@ ya si es una funcion que cambie todo al mismo tiempo, pues no creo que sea lo ma
 import { StateGame } from "../game/stateGame.js";
 
 export class GameInformationDisplay {
+    // Actualiza/crea TODA la informacion de primerazo
     static updateAllInformation() {
         const statePlayers = document.getElementById("state-players");
         statePlayers.innerHTML = `
@@ -18,5 +19,22 @@ export class GameInformationDisplay {
                 <p class="game-stat-text">Turno <span id="value-current-turn">${StateGame.getTurn()}</span></p>
             </div>
         `
+    }
+    // Todas las funciones desde ahora en adelante debe de ser usado DESPUES de que se garantize de que los elementos con las estadisticas existan
+
+    // Actualiza el valor de los carros actuales, dependiendo del jugador que sea
+    // Se pone el respectivo jugador (player1 o player2), y lo cambia dependiendo del nuevo valor que esta en .getCarsPlayers1()
+    static updateCurrentCarsPlayer(player) {
+        const currentCarsValue = document.getElementById(`value-cars-current-${player}`);
+        (player === "player1") ? (currentCarsValue.innerText = StateGame.getCarsPlayers1()) : (currentCarsValue.innerText = StateGame.getCarsPlayers2());
+    }
+
+    // Actualiza el valor de la ronda con el valor de .getRound()
+    static updateCurrentRound() {
+        document.getElementById("value-current-round").innerText = StateGame.getRound();
+    }
+    // Actualiza el valor del turno con el valor de .getTurn()
+    static updateCurrentTurn() {
+        document.getElementById("value-current-turn").innerText= StateGame.getTurn();
     }
 }
