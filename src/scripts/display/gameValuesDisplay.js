6507e123200109a3409eabd854f85c0e52c12831
Bedoya19 <108559038+Bedoya19@users.noreply.github.com>
2026-01-05 12:17:23 -0500

    updateAllValues ahora cambia los valores dependiendo del jugador

--- src/scripts/display/gameValuesDisplay.js
+++ src/scripts/display/gameValuesDisplay.js
@@ -4,17 +4,19 @@
 */
 import { Player1 } from "../players/player1";
 import { Player2 } from "../players/player2";
+import { PlayerActions } from "../players/playerActions";
 
 export class gameValuesDisplay {
     // Funcion bastante parecida a updateAllInformation, en donde cambia en su totalidad todo el elemento de los valores locales
     static updateAllValues(player) {
+        // Cuando se agrege la funcionalidad de los ataques, esto tendra que cambias
         document.getElementById("game-values").innerHTML = `
             <h3 class="game-stat-text game-stat-subtitle-text">Valores:</h3>
             <h3 class="game-stat-text">Actual: <span id="value-locar-player">${player}</span></h3>
             <ul id="game-value-content">
                 <p class="game-stat-text">Carga: <span id="value-local-charge">6</span>-6</p>
-                <p class="game-stat-text">Nitro: <span id="value-local-nitro">5</span></p>
-                <p class="game-stat-text">Poder: <span id="value-local-power">5</span></p>
+                <p class="game-stat-text">Nitro: <span id="value-local-nitro">${PlayerActions.getNitroFromPlayer(player)}</span></p>
+                <p class="game-stat-text">Poder: <span id="value-local-power">${PlayerActions.getPowerFromPlayer(player)}</span></p>
             </ul>
         `;
     }
