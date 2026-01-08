d6057f5101572530a7ab9143197ef91b0f9628a6
Bedoya19 <108559038+Bedoya19@users.noreply.github.com>
2026-01-08 12:08:47 -0500

    agregado mas atributos a AttackValues mas refractorizado los setters de los valores

--- src/scripts/battle/attackValues.js
+++ src/scripts/battle/attackValues.js
@@ -4,6 +4,8 @@ export class AttackValues {
     static #prepareAttack = false;
     static #charge = 0;
     static #attack = 0;
+    static #zone = 0;
+    static #squareNumber = 0;
 
     // Getters
     static getCharge() {
@@ -15,16 +17,27 @@ export class AttackValues {
     static getPrepareAttack() {
         return this.#prepareAttack;
     }
+    static getZone() {
+        return this.#zone;
+    }
+    static getSquareNumber() {
+        return this.#squareNumber;
+    }
 
     // Define los valores de carga y ataque
     static resetAttackValues() {
         this.#prepareAttack = false;
-        this.#charge = 0;
-        this.#attack = 0;
+        this.setValues(0, 0, 0, 0);
     }
-    static setAttackValues(charge, attack) {
+    static setAttackValues(charge, attack, zone, squareNumber) {
         this.#prepareAttack = true;
+        this.setValues(charge, attack, zone, squareNumber);
+    }
+    // Para hacer esto un poco mas estandar
+    static setValues(charge, attack, zone, squareNumber) {
         this.#charge = charge;
         this.#attack = attack;
+        this.#zone = zone;
+        this.#squareNumber = squareNumber;
     }
 }
