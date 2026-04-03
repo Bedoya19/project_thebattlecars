287baa2df02cf0f2e9587fedbdcbba2f1e81ba5f
Bedoya19 <108559038+Bedoya19@users.noreply.github.com>
2026-04-03 12:01:59 -0500

    Agregado funciones de cargar la data de los jsons usando loadCardJSON

--- src/scripts/models/cards/loadCardsJSON.js
+++ src/scripts/models/cards/loadCardsJSON.js
@@ -4,11 +4,24 @@
 export class LoadCardsJSON {
     static async loadCardJSON(jsonFile) {
         try {
-            const res = await fetch("src/scripts/cardsData/cars.json");
+            // Deberia de tener el jsonfile el .json
+            const res = await fetch(`src/scripts/cardsData/${jsonFile}`);
             if (!res.ok) throw new Error(`HTTP Error ${res.status}`);
             return await res.json();
         } catch (e) {
             console.error(`Error ${e}`);
         }
+    }
+    // Funciones que devuelven la data de los jsons individual
+    static async loadCarsFromJSON() {
+        return await this.loadCardJSON("cars.json");
+    }
+
+    static async loadWeaponsFromJSON() {
+        return await this.loadCardJSON('weapons.json');
+    }
+
+    static async loadMaterialsFromJSON() {
+        return await this.loadCardJSON('materials.json');
     }
 }
