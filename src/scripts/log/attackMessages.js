5d0496a479f1258b1bd2ca4f0c7eaea99f12da86
Bedoya19 <108559038+Bedoya19@users.noreply.github.com>
2026-06-16 21:21:03 -0500

    Creado funcion que genera un mensaje generico cuando se ataca con un arma, pasarlo a las notas del juego

--- src/scripts/log/attackMessages.js
+++ src/scripts/log/attackMessages.js
@@ -1,12 +1,19 @@
 // Script para la generacion de mensajes de ataque 
 // No solo para tenerlos en un ambiente mas estandar y ser reutilizado en otros lugares. Quien sabe para que mas lo podria usar.
+import { DisplayCardInformation } from "../display/displayCardInformation"
+
 
 export class AttackMessages {
+    // Genera un mensaje corto sobre que X carro ataco a Y carro
+    static carAttacks(carAttackerSquare, carAttackedSquare, weaponSquare, charge) {
+        return `El ${DisplayCardInformation.convertPlayerString(carAttackerSquare.id.slice(0, 7))} ataca a ${carAttackedSquare.dataset.name} usando su ${carAttacker.dataset.name} y el arma ${weaponSquare.dataset.name} teniendo un ${charge}/6 de carga`;
+    }
+
     // Genera lo que le pasa al carro al ser destruido
     static carDestroyedMessage(carAttackedElement, carDestroyed, charge, attack) {
         return [
             `${carAttackedElement.dataset.name} fue atacado por un ataque de carga ${charge}, que cause un daño de ${attack}. ¡Suficiente para destruirlo!`,
-            `Le quedarian al ${this.convertPlayerString(carAttackedElement.id.slice(0,7))} ${StateGame.geCarsFromPlayer(carAttackedElement.id.slice(0,7)) - 1} carros restantes`,
+            `Le quedarian al ${DisplayCardInformation.convertPlayerString(carAttackedElement.id.slice(0,7))} ${StateGame.geCarsFromPlayer(carAttackedElement.id.slice(0,7)) - 1} carros restantes`,
         ]
     }
 }
