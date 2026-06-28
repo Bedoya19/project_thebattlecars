e914d4372720959ec514fc30a57f5172345988f8
Bedoya19 <108559038+Bedoya19@users.noreply.github.com>
2026-06-28 13:17:35 -0500

    Agregado funcion que crea el boton de nitro (todavia ni idea si funciona

--- src/scripts/battle/nitro.js
+++ src/scripts/battle/nitro.js
@@ -3,8 +3,22 @@
     Aqui funcionara todas las funciones necesarias para el nitro de los carros.
     Esto incluye la activacion del nitro, revisar los activados, todo eso.
 */
+import { StandarizedDocCreation } from "../display/standardDoc/standarizedDocCreation.js";
 
 export class Nitro {
     // Agrega el boton de activar nitro
-    
+    // Ahora que lo pienso... Es bastante similar al otro de ataque... Hmmmm...
+    static createNitroButton(player, zoneNumber, squareNumber, carSquare) {
+        // Toca estandarizar esto del jugador
+        const currentPlayer = document.getElementById("deck").dataset.player;
+        if (currentPlayer === player) {
+            const button = StandarizedDocCreation.customElementCreator(
+                {
+                    "element": "button",
+                    "id": `nitro-${zoneNumber}-${squareNumber}`,
+                    "class": "nitro-button"
+                }
+            )
+        }
+    }
 }
