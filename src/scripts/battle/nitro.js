5981a721e82bc5121b02f50f5613a331b1599834
Bedoya19 <108559038+Bedoya19@users.noreply.github.com>
2026-06-28 12:59:58 -0500

    Creado script de Nitro, comenzado trabajos para este

---
+++ src/scripts/battle/nitro.js
@@ -0,0 +1,10 @@
+/* 
+    Metodos del nitro de los carros
+    Aqui funcionara todas las funciones necesarias para el nitro de los carros.
+    Esto incluye la activacion del nitro, revisar los activados, todo eso.
+*/
+
+export class Nitro {
+    // Agrega el boton de activar nitro
+    
+}
