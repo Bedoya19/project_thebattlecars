93f7dbe005df18ee147e42367dda3074012b45f4
Bedoya19 <108559038+Bedoya19@users.noreply.github.com>
2026-06-29 16:05:43 -0500

    El boton de nitro se desactiva si no se tiene suficiente nitro

--- src/scripts/battle/nitro.js
+++ src/scripts/battle/nitro.js
@@ -24,6 +24,7 @@ export class Nitro {
             button.addEventListener("click", () => {
                 this.activateNitro(player, carSquare)
             });
+            !this.isNitroActivate(player, carSquare) ? button.disabled = true : button.disabled = false;
             return button;
         } else {
             return document.createElement("div");
@@ -32,10 +33,15 @@ export class Nitro {
 
     static activateNitro(player, carSquare) {
         console.log(carSquare);
-        if (PlayerActions.getNitroFromPlayer(player) >= carSquare.dataset.nitroQuantity) {
+        if (this.isNitroActivate(player, carSquare)) {
             console.log("Se va a activar el nitro...");
         } else {
             console.log("No se puede activar el nitro...");
         }
+    }
+
+    // Una simple funcion que sirve para revisar si el nitro puede ser activado
+    static isNitroActivate(player, carSquare) {
+        return PlayerActions.getNitroFromPlayer(player) >= carSquare.dataset.nitroQuantity;
     }
 }
